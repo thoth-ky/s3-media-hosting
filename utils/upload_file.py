0a9064c8557fa06ca6e27eda9750c7d62b64cdfc
Thoth Ky <28805113+thoth-ky@users.noreply.github.com>
2021-02-08 16:04:25 +0300

    ch: s3 upload util

---
+++ utils/upload_file.py
@@ -0,0 +1,47 @@
+import logging
+import boto3
+from botocore.exceptions import ClientError
+from decouple import config
+
+
+def upload_file_object(file_obj, bucket, key):
+    """Upload a file to an S3 bucket
+
+    :param file_obj:  A file-like object to upload. At a minimum, it must implement the read method, and must return bytes.
+    :param bucket: Bucket to upload to
+    :param key: name of key to upload to
+    :param callback_fxn: function to process response after upload
+    :return: true if file was uploaded, else False
+    """
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_fileobj(file_obj, bucket, key)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
+if __name__ == "__main__":
+    logger = logging.getLogger()
+    logger.info('TEST RUN STARTED')
+    test_file = 'test.png'
+    BUCKET_NAME = config('S3_BUCKET_NAME')
+    KEY_NAME = 'apps/bs/test.png'
+
+
+    with open(test_file, 'rb') as image:
+        response = upload_file_object(
+            file_obj=image,
+            bucket=BUCKET_NAME,
+            key=KEY_NAME,
+        )
+
+        if response:
+            print("Successful")
+        print("Failed", response)
+
+    logger.info('TEST RUN COMPLETED')
+    
